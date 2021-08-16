470f290a47c649f46b4aff10051075a59160267d
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-08-16 17:47:04 -0300

    Making the navbar responsive

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,5 +1,29 @@
 <template>
-    <div class="navbar shadow-lg bg-neutral text-neutral-content">
+
+<nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+  </div>
+
+  <label class="block lg:hidden cursor-pointer flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" for="menu-toggle"><svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg></label>
+  <input class="hidden" type="checkbox" id="menu-toggle" />
+
+  <div class="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="menu">
+    <div class="text-sm lg:flex-grow">
+      <router-link to="/home" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Home
+      </router-link>
+      <router-link to="/table" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Table View
+      </router-link>
+      <router-link to="/panel" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Panel View
+      </router-link>
+    </div>
+  </div>
+</nav>
+
+<!--     <div class="navbar shadow-lg bg-neutral text-neutral-content">
   <div class="px-2 mx-2 navbar-start">
     <span class="text-lg font-bold">
             DaisyUI
@@ -24,5 +48,11 @@
       </svg>
     </button>
   </div>
-</div>
+</div> -->
 </template>
+
+<style scoped>
+#menu-toggle:checked+#menu {
+  display: block;
+}
+</style>
