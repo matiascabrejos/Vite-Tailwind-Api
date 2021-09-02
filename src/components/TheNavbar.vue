25fa1110abfe0dee12385f9de80000e6ff1bb4ee
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-09-01 22:27:38 -0300

    Working in the last features

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,7 +2,7 @@
 
 <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
   <div class="flex items-center flex-shrink-0 text-white mr-6">
-    <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+    <span class="font-semibold text-xl tracking-tight">Crypto App</span>
   </div>
 
   <label class="block lg:hidden cursor-pointer flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" for="menu-toggle"><svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg></label>
@@ -10,13 +10,13 @@
 
   <div class="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="menu">
     <div class="text-sm lg:flex-grow">
-      <router-link to="/home" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent mr-4">
+      <router-link to="/home" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent mr-4 text-base">
         Home
       </router-link>
-      <router-link to="/table" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent mr-4">
+      <router-link to="/table" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent mr-4 text-base">
         Table View
       </router-link>
-      <router-link to="/panel" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent">
+      <router-link to="/panel" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-accent text-base">
         Panel View
       </router-link>
     </div>
@@ -27,6 +27,10 @@
 <style scoped>
 #menu-toggle:checked+#menu {
   display: block;
+}
+
+a.active {
+  @apply text-accent;
 }
 
 
